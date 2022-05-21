48e63d1663748af4e720ed48f9702d46f46930d7
xxxRichiexxx <xxxRichiexxx@yandex.ru>
2022-05-21 23:57:56 +0300

    ревью v.1

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '51.250.106.127']
 INTERNAL_IPS = [
