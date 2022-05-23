de043dcb2edded20e5f2b81b80faa306c76fcf60
xxxRichiexxx <xxxRichiexxx@yandex.ru>
2022-05-23 21:41:16 +0300

    Ревью 2

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -45,19 +45,19 @@ class RecipeViewSet(viewsets.ModelViewSet):
         'partial_update': RecipeCreateSerializer,
     }
 
-    def get_queryset(self):
-        is_favorited_query = Recipe.objects.filter(
-            id=OuterRef('pk'),
-            connoisseurs__id=self.request.user.id
-        )
-        is_in_shopping_cart_query = Recipe.objects.filter(
-            id=OuterRef('pk'),
-            buyers__id=self.request.user.id
-        )
-        return Recipe.objects.annotate(
-            is_favorited=Exists(is_favorited_query),
-            is_in_shopping_cart=Exists(is_in_shopping_cart_query)
-        )
+    # def get_queryset(self):
+    #     is_favorited_query = Recipe.objects.filter(
+    #         id=OuterRef('pk'),
+    #         connoisseurs__id=self.request.user.id
+    #     )
+    #     is_in_shopping_cart_query = Recipe.objects.filter(
+    #         id=OuterRef('pk'),
+    #         buyers__id=self.request.user.id
+    #     )
+    #     return Recipe.objects.annotate(
+    #         is_favorited=Exists(is_favorited_query),
+    #         is_in_shopping_cart=Exists(is_in_shopping_cart_query)
+    #     )
 
     def get_serializer_class(self):
         return self.SERIALIZERS[self.action]
