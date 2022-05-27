63803e1bac7be1deb1cb3a5d7e19fe5793036e2b
xxxRichiexxx <xxxRichiexxx@yandex.ru>
2022-05-27 21:15:26 +0300

    Ревью 2

--- backend/ingredients/filters.py
+++ backend/ingredients/filters.py
@@ -1,4 +1,5 @@
 from rest_framework import filters
+from django.db.models import Q
 
 
 class IngredientsSearchFilter(filters.BaseFilterBackend):
@@ -6,7 +7,5 @@ class IngredientsSearchFilter(filters.BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
         name = request.GET.get('name')
         if name:
-            part_one = queryset.filter(name__istartswith=name)
-            part_two = queryset.filter(name__icontains=name)
-            return part_two.union(part_one)[:50]
+            return queryset.filter(Q(name__istartswith=name))[:50]
         return queryset
