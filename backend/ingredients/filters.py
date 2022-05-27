6ee5fc6b044a94a7fb2857fcb100f269648a7c50
xxxRichiexxx <xxxRichiexxx@yandex.ru>
2022-05-27 22:48:30 +0300

    Ревью 2

--- backend/ingredients/filters.py
+++ backend/ingredients/filters.py
@@ -1,5 +1,6 @@
 from rest_framework import filters
 # from django.db.models import Q
+from django.db.models import Case, Value, When
 
 
 class IngredientsSearchFilter(filters.BaseFilterBackend):
@@ -7,13 +8,19 @@ class IngredientsSearchFilter(filters.BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
         name = request.GET.get('name')
         if name:
-            part_one = queryset.filter(name__istartswith=name)
-            part_two = queryset.filter(
-                name__icontains=name
-            ).exclude(name__istartswith=name)
+            result = queryset.filter(name__icontains=name).annotate(
+                Case(
+                    When(name__istartswith=name, then=Value(0)),
+                    default=Value(1),
+                )
+            )
+            # part_one = queryset.filter(name__istartswith=name)
+            # part_two = queryset.filter(
+            #     name__icontains=name
+            # ).exclude(name__istartswith=name)
             # return part_one.union(part_two).order_by()[:50]
-            return part_one | part_two
+            # return part_one | part_two
             # return queryset.filter(
             #     Q(name__istartswith=name) | Q(name__icontains=name)
             # )[:50]
-        return queryset
+        return result.order_by('ordering_index', 'name')
