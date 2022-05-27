1f48ac07b03e302dbd58fab97663c84443f01bbd
xxxRichiexxx <xxxRichiexxx@yandex.ru>
2022-05-27 23:11:03 +0300

    Ревью 2

--- backend/ingredients/filters.py
+++ backend/ingredients/filters.py
@@ -1,6 +1,6 @@
 from rest_framework import filters
 # from django.db.models import Q
-from django.db.models import Case, Value, When
+from django.db.models import Case, Value, When, IntegerField
 
 
 class IngredientsSearchFilter(filters.BaseFilterBackend):
@@ -12,6 +12,7 @@ class IngredientsSearchFilter(filters.BaseFilterBackend):
                 Case(
                     When(name__istartswith=name, then=Value(0)),
                     default=Value(1),
+                    output_field=IntegerField()
                 )
             )
             # part_one = queryset.filter(name__istartswith=name)
