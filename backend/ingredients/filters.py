090229f338011f509637f30d7b0bd3db3ee63058
xxxRichiexxx <xxxRichiexxx@yandex.ru>
2022-05-27 21:29:48 +0300

    Ревью 2

--- backend/ingredients/filters.py
+++ backend/ingredients/filters.py
@@ -7,5 +7,7 @@ class IngredientsSearchFilter(filters.BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
         name = request.GET.get('name')
         if name:
-            return queryset.filter(Q(name__istartswith=name)|Q(name__icontains=name))[:50]
+            return queryset.filter(
+                Q(name__istartswith=name) | Q(name__icontains=name)
+            )[:50]
         return queryset
